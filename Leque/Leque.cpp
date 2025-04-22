2563ee91e54930b9cdba10b9e3b4e80e2f627c28
Skull33 <alonzo.cuela@ucsp.edu.pe>
2025-04-22 09:16:34 -0500

    AED v0.17

--- Leque/Leque.cpp
+++ Leque/Leque.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+template<class O>
+void print(O* arr, O tam)
+{
+	for (O* i = arr + 0; i <= arr + tam - 1; i++)
+	{
+		cout << *i << " ";
+	}
+	cout << endl;
+}
+
 template <class T>
 class Leque
 {
@@ -9,16 +19,35 @@ public:
 	Leque(int tamano):tam(tamano)
 	{
 		mapa = new T* [tam];
-		mapa[0] = &mapeador;
+		mapeador = mapa + 0;
+		ptrarr = new T[tam];
+		mapa[i] = ptrarr;
+		ptr = ptrarr + 0;
 	}
 	void PushFront(T valor)
 	{
-		mapeador = new T[tam];
 		if (mapeador < mapa + tam - 1)
 		{
-			*mapeador = valor;
-			cout << *mapeador << endl;
-		} 
+			if (ptr < ptrarr + tam - 1)
+			{
+				for (T* p = ptrarr + tam - 1; p >= ptrarr; p--)
+				{
+					T* q = p - 1;
+					*p = *q;
+				}
+				*ptr = valor;
+				print<int>(ptrarr, tam);
+			}
+			else
+			{
+				mapeador++;
+				i = i + 1;
+				T* narr;
+				narr = new T[tam];
+				mapa[i] = narr;
+				ptrarr = narr;
+			}
+		}
 	}
 	/*
 	void PopFront(& valor);
@@ -29,7 +58,10 @@ public:
 
 private:
 	T** mapa;
-	T* mapeador;
+	T** mapeador;
+	T* ptrarr;
+	T* ptr;
+	int i = 0;
 	int tam;
 };
 
@@ -37,4 +69,5 @@ int main()
 {
 	Leque<int>L1{ 5 };
 	L1.PushFront(12);
+	L1.PushFront(14);
 }
