845e5cd54ef41801e753529f7698283266f585af
Skull33 <alonzo.cuela@ucsp.edu.pe>
2025-04-29 08:10:24 -0500

    AED v0.19 update

--- lista ordenada con Nodos/lista ordenada con Nodos/Soter-Forward-List.cpp
+++ lista ordenada con Nodos/lista ordenada con Nodos/Soter-Forward-List.cpp
@@ -16,72 +16,72 @@ struct CNode
 class CList
 {
 public:
-	CList()
-	{
-		head = 0;
-	}
-	~CList()
-	{
-
-	}
-	void print()
-	{
-		for (CNode* T = head; T; T = T->next)
-		{
-			cout << T->value << " ";
-		}
-		cout << endl;
-	}
-	bool Find(int val, CNode**& p)
-	{
-		for (p = &head;*p && (*p)->value < val;p = &((*p)->next))
-		{
-			return *p && (*p)->value == val;
-		}
-	}
-	bool Insert(int val)
-	{
-		CNode** p;
-		if (Find(val, p))
-		{
-			return false;
-		}
-		else
-		{
-			CNode* temp = new CNode(val);
-			temp->next = *p;
-			*p = temp;
-			return true;
-		}
-	}
-	bool Remove(int val)
-	{
-		CNode** p;
-		if (!Find(val, p))
-		{
-			return false;
-		}
-		else
-		{
-			CNode* temp = *p;
-			*p = temp->next;
-			delete temp;
-			return true;
-		}
-	}
+	CList();
+	~CList();
+	bool Find(int x, CNode**& p);
+	bool Insert(int val);
+	bool Remove(int val);
+	void print();
 private:
 	CNode* head;
 };
 
+CList::CList()
+{
+	head = 0;
+}
+
+CList::~CList()
+{}
+
+bool CList::Find(int x, CNode**& p)
+{
+	//solo hace el avance del puntero
+	for (p = &head; *p && (*p)->value < x; p = &((*p)->next));
+	//esto retorna 0 o 1
+	return *p && (*p)->value == x;
+}
+
+bool CList::Insert(int val)
+{
+	CNode** p;
+	if (Find(val, p)) return false;
+	CNode* temp = new CNode(val);
+	temp->next = *p;
+	*p = temp;
+	return true;
+}
+
+bool CList::Remove(int val)
+{
+	CNode** p;
+	if (!Find(val, p)) return false;
+	CNode* temp = *p;
+	*p = temp->next;
+	delete temp;
+	return true;
+}
+
+void CList::print()
+{
+	for (CNode* t = head; t; t = t->next)
+	{
+		cout << t->value << " ";
+	}
+	cout << "\n";
+}
+
 int main()
 {
 	CList lista;
-	lista.Insert(2);
-	lista.Insert(11);
-	lista.Insert(60);
 	lista.Insert(4);
-	lista.Insert(3);
+	lista.Insert(2);
+	lista.Insert(8);
 	lista.print();
-
+	lista.Remove(2);
+	lista.print();
+	lista.Insert(95);
+	lista.Insert(1);
+	lista.print();
 	return 0;
 }
